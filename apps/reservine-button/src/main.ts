b92f479243f5c683c56480c58a653ec494612a31
Lukáš Přibík <lukas.pribik96@gmail.com>
2025-02-11 23:13:37 +0100

    ref: cleanup

--- apps/reservine-button/src/main.ts
+++ apps/reservine-button/src/main.ts
@@ -1,17 +1,1 @@
-/*
-import App from './App.svelte';
-*/
-
 import './reservine-button.component.svelte';
-//import './reservine-widget.component.svelte';
-
-/*
-const app = new App({
-  target: document.body,
-  props: {
-    name: 'reservine-button',
-  },
-});
-
-export default app;
-*/
